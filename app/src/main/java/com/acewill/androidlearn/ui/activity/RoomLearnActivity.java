9b1afa79fd800eb2f702c14656b95cbbffc83b99
liyx <liyx@acewill.cn>
2020-11-05 19:40:10 +0800

    数据库页面增加Button监听；

--- app/src/main/java/com/acewill/androidlearn/ui/activity/RoomLearnActivity.java
+++ app/src/main/java/com/acewill/androidlearn/ui/activity/RoomLearnActivity.java
@@ -1,12 +1,37 @@
 package com.acewill.androidlearn.ui.activity;
 
+import android.view.View;
+
+import com.acewill.androidlearn.R;
 import com.acewill.androidlearn.databinding.ActivityRoomLearnBinding;
 import com.acewill.androidlearn.ui.base.BaseActivity;
 
-public class RoomLearnActivity extends BaseActivity<ActivityRoomLearnBinding> {
+public class RoomLearnActivity extends BaseActivity<ActivityRoomLearnBinding> implements View.OnClickListener {
 
     @Override
     protected void initView() {
 
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_add:
+
+                break;
+            case R.id.btn_remove:
+
+                break;
+            case R.id.btn_update:
+
+                break;
+            case R.id.btn_query:
+
+                break;
+            default:
+                break;
+        }
     }
 }
